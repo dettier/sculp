7e32eafb3229b3c7ac547371b56b49e8a708a454
dettier <sergey.tychinin@gmail.com>
2016-08-10 16:52:19 +0500

    custom validations support

---
+++ test/custom-validations.js
@@ -0,0 +1,43 @@
+/* eslint-env mocha */
+////////////////////////////////////////////////////////////////////////////////
+// REQUIRES : BEGIN
+////////////////////////////////////////////////////////////////////////////////
+
+import { assert } from 'chai';
+import startsWith from 'underscore.string/startsWith';
+
+import { validate } from '../lib/index';
+import { TYPE } from '../lib/enums';
+
+////////////////////////////////////////////////////////////////////////////////
+// REQUIRES : END
+////////////////////////////////////////////////////////////////////////////////
+
+describe('Custom validations:', function () {
+
+  const scheme = {
+    type : TYPE.STRING,
+    $startsWith : 'AB'
+  };
+
+  it('should throw if custom validation is not registered', function () {
+    assert.throws(() => validate(5, scheme), 'Unknown validation startsWith');
+  });
+
+  it('should use custom validation if it\'s provided', function () {
+
+    const options = {
+      validations : {
+        startsWith (fa, ruleValue) {
+          if (startsWith(fa() || '', ruleValue) === false)
+            return `value should start with ${ruleValue}`;
+          return undefined;
+        }
+      }
+    };
+
+    assert.equal(validate('ABab', scheme, options), 'ABab');
+    assert.throws(() => validate('CDcd', scheme, options), 'value should start with AB');
+  });
+
+});
